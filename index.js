1aece45cbb11963d45c16d67d4d3554dd7a4894a
guohui.deng <guohui.deng@brilliantaero.com>
2017-11-29 19:19:27 +0800

    使用prop-types检查属性

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ import {
     Text,
 } from 'react-native'
 
+import PropTypes from 'prop-types';
+
 export const DURATION = { 
     LENGTH_LONG: 2000, 
     LENGTH_SHORT: 500,
@@ -133,17 +135,17 @@ const styles = StyleSheet.create({
 });
 
 Toast.propTypes = {
-    style: View.propTypes.style,
-    position: React.PropTypes.oneOf([
+    style: PropTypes.any,
+    position: PropTypes.oneOf([
         'top',
         'center',
         'bottom',
     ]),
-    textStyle: Text.propTypes.style,
-    positionValue: React.PropTypes.number,
-    fadeInDuration: React.PropTypes.number,
-    fadeOutDuration: React.PropTypes.number,
-    opacity: React.PropTypes.number
+    textStyle: propTypes.any,
+    positionValue: PropTypes.number,
+    fadeInDuration: PropTypes.number,
+    fadeOutDuration: PropTypes.number,
+    opacity: PropTypes.number
 }
 
 Toast.defaultProps = {
