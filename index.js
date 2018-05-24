d7f09ac8e2d560514f029f7028043da790b93129
iagor moraes <iagor@devsquad.com>
2018-05-24 17:27:11 -0300

    added possibility to customize pointer events
    - auto indent via prettier

--- index.js
+++ index.js
@@ -6,34 +6,26 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {
-    StyleSheet,
-    View,
-    Animated,
-    Dimensions,
-    Text,
-    ViewPropTypes as RNViewPropTypes,
-} from 'react-native'
+import React, { Component } from 'react';
+import { StyleSheet, View, Animated, Dimensions, Text, ViewPropTypes as RNViewPropTypes } from 'react-native';
 
 import PropTypes from 'prop-types';
 const ViewPropTypes = RNViewPropTypes || View.propTypes;
-export const DURATION = { 
+export const DURATION = {
     LENGTH_SHORT: 500,
     FOREVER: 0,
 };
 
-const {height, width} = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 
 export default class Toast extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
             isShow: false,
             text: '',
             opacityValue: new Animated.Value(this.props.opacity),
-        }
+        };
     }
 
     show(text, duration, callback) {
@@ -44,38 +36,32 @@ export default class Toast extends Component {
             text: text,
         });
 
-        Animated.timing(
-            this.state.opacityValue,
-            {
-                toValue: this.props.opacity,
-                duration: this.props.fadeInDuration,
-            }
-        ).start(() => {
+        Animated.timing(this.state.opacityValue, {
+            toValue: this.props.opacity,
+            duration: this.props.fadeInDuration,
+        }).start(() => {
             this.isShow = true;
-            if(duration !== DURATION.FOREVER) this.close();
+            if (duration !== DURATION.FOREVER) this.close();
         });
     }
 
-    close( duration ) {
+    close(duration) {
         let delay = typeof duration === 'undefined' ? this.duration : duration;
 
-        if(delay === DURATION.FOREVER) delay = this.props.defaultCloseDelay || 250;
+        if (delay === DURATION.FOREVER) delay = this.props.defaultCloseDelay || 250;
 
         if (!this.isShow && !this.state.isShow) return;
         this.timer && clearTimeout(this.timer);
         this.timer = setTimeout(() => {
-            Animated.timing(
-                this.state.opacityValue,
-                {
-                    toValue: 0.0,
-                    duration: this.props.fadeOutDuration,
-                }
-            ).start(() => {
+            Animated.timing(this.state.opacityValue, {
+                toValue: 0.0,
+                duration: this.props.fadeOutDuration,
+            }).start(() => {
                 this.setState({
                     isShow: false,
                 });
                 this.isShow = false;
-                if(typeof this.callback === 'function') {
+                if (typeof this.callback === 'function') {
                     this.callback();
                 }
             });
@@ -100,17 +86,17 @@ export default class Toast extends Component {
                 break;
         }
 
-        const view = this.state.isShow ?
-            <View
-                style={[styles.container, { top: pos }]}
-                pointerEvents="none"
-            >
-                <Animated.View
-                    style={[styles.content, { opacity: this.state.opacityValue }, this.props.style]}
-                >
-                    {React.isValidElement(this.state.text) ? this.state.text : <Text style={this.props.textStyle}>{this.state.text}</Text>}
+        const view = this.state.isShow ? (
+            <View style={[styles.container, { top: pos }]} pointerEvents={this.props.pointerEvents}>
+                <Animated.View style={[styles.content, { opacity: this.state.opacityValue }, this.props.style]}>
+                    {React.isValidElement(this.state.text) ? (
+                        this.state.text
+                    ) : (
+                        <Text style={this.props.textStyle}>{this.state.text}</Text>
+                    )}
                 </Animated.View>
-            </View> : null;
+            </View>
+        ) : null;
         return view;
     }
 }
@@ -130,23 +116,20 @@ const styles = StyleSheet.create({
         padding: 10,
     },
     text: {
-        color: 'white'
-    }
+        color: 'white',
+    },
 });
 
 Toast.propTypes = {
     style: ViewPropTypes.style,
-    position: PropTypes.oneOf([
-        'top',
-        'center',
-        'bottom',
-    ]),
+    position: PropTypes.oneOf(['top', 'center', 'bottom']),
     textStyle: Text.propTypes.style,
-    positionValue:PropTypes.number,
-    fadeInDuration:PropTypes.number,
-    fadeOutDuration:PropTypes.number,
-    opacity:PropTypes.number
-}
+    positionValue: PropTypes.number,
+    fadeInDuration: PropTypes.number,
+    fadeOutDuration: PropTypes.number,
+    opacity: PropTypes.number,
+    pointerEvents: PropTypes.string,
+};
 
 Toast.defaultProps = {
     position: 'bottom',
@@ -154,5 +137,6 @@ Toast.defaultProps = {
     positionValue: 120,
     fadeInDuration: 500,
     fadeOutDuration: 500,
-    opacity: 1
-}
+    opacity: 1,
+    pointerEvents: 'none',
+};
