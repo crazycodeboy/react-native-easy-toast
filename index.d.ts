183c753d6ba30675f2796bc4c2b2f19e951e069f
Ryoga Kitagawa <RyogaK@users.noreply.github.com>
2019-08-05 12:48:01 +0900

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,26 @@
 import { Component, ReactNode } from "react";
+import { StyleProp, TextStyle, ViewStyle } from "react-native";
+
+interface ToastViewProps {
+  style?: StyleProp<ViewStyle>;
+  position?: "top" | "center" | "bottom";
+  textStyle?: StyleProp<TextStyle>;
+  positionValue?: number;
+  fadeInDuration?: number;
+  fadeOutDuration?: number;
+  opacity?: number;
+}
+
+interface DURATION {
+  LENGTH_SHORT: number;
+  FOREVER: number;
+}
 
 declare module "react-native-easy-toast" {
-  export interface DURATION {
-    LENGTH_SHORT: number;
-    FOREVER: number;
-  }
-  export default class Toast extends Component {
-    show: (
-      text: string | ReactNode,
-      duration?: number,
-      callback?: (() => void)
-    ) => void;
+  export const DURATION: DURATION;
+
+  export default class Toast extends Component<ToastViewProps> {
+    show: (text: string | ReactNode, duration?: number, callback?: () => void) => void;
     close: (duration?: number) => void;
   }
 }
